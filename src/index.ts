5fb8c6b346d2ce4248316aa76f523b4cddde3202
rakannimer <rakannimer@gmail.com>
2019-06-12 12:43:21 +0300

    Better type inference

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-function get<I, O>(obj: I, reducer: (obj: I) => O, defaultTo?: O) {
+export function get<O, I>(
+  obj: I,
+  reducer: (obj: Exclude<I, undefined | null>) => O,
+  defaultTo: O
+) {
+  type ReturnType = Exclude<O, undefined | null>;
   try {
-    const returnValue = reducer(obj);
+    const returnValue = reducer(obj as Exclude<I, undefined | null>);
     if (returnValue === undefined || returnValue === null) {
-      return defaultTo;
+      return defaultTo as ReturnType;
     } else {
-      return returnValue;
+      return returnValue as ReturnType;
     }
   } catch (err) {
-    return defaultTo;
+    return defaultTo as ReturnType;
   }
 }
 
